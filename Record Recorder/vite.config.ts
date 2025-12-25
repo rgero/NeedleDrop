da8edd32c8b3f5b9e5c8e74277ef81075681f863
Roy Gero <roy.gero.ii@gmail.com>
2025-12-24 21:24:01 -0500

    Installing some dependencies and adding resolve paths

--- Record Recorder/vite.config.ts
+++ Record Recorder/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
 })
