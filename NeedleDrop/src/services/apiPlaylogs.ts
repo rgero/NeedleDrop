8e4d77b9589ee0e644171955012e6f0e5ba86360
Roy Gero <roy.gero.ii@gmail.com>
2026-01-18 20:46:45 -0500

    Updating the functions to create/update the playlogs so that the date isn't inaccurately set.

--- NeedleDrop/src/services/apiPlaylogs.ts
+++ NeedleDrop/src/services/apiPlaylogs.ts
@@ -1,5 +1,4 @@
 import type { PlayLog } from "@interfaces/PlayLog";
-import { format } from "date-fns";
 import supabase from "./supabase";
 
 export const getPlaylogs = async () => {
@@ -27,7 +26,7 @@ export const createPlaylog = async (newItem: Omit<PlayLog, 'id'>) => {
   const { data, error } = await supabase.from('playlogs').insert([
     {
       ...newItem,
-      date: newItem.date ? format(newItem.date, "yyyy-MM-dd") : null,
+      date: newItem.date ?? null,
       listeners: newItem.listeners?.map(u => u.id) ?? []
     }
   ]).select().single(); 
@@ -44,7 +43,7 @@ export const createPlaylog = async (newItem: Omit<PlayLog, 'id'>) => {
 export const updatePlaylog = async (id: number, updatedItem: Partial<PlayLog>) => {
   const { data, error } = await supabase.from('playlogs').update({
     ...updatedItem,
-    date: updatedItem.date ? format(updatedItem.date, "yyyy-MM-dd")  : null,
+    date: updatedItem.date ?? null,
     listeners: updatedItem.listeners?.map(u => u.id) ?? []
   }).eq('id', id).select().single();
 
