34beea6d47ab087be4f8ebb3cbccd9ba8edec712
Roy Gero <roy.gero.ii@gmail.com>
2026-01-27 11:15:58 -0500

    #11 - Adding the settings to the Vinyl table.

--- NeedleDrop/src/components/vinyls/VinylsTable.tsx
+++ NeedleDrop/src/components/vinyls/VinylsTable.tsx
@@ -1,30 +1,41 @@
-import { DataGrid, type GridRowClassNameParams } from "@mui/x-data-grid";
+import { DataGrid, type GridColumnVisibilityModel, type GridRowClassNameParams } from "@mui/x-data-grid";
 import { VinylTableColumnDef } from "./VinylsTableColumnDef";
 import { useNavigate } from "react-router-dom";
 import { useVinylContext } from "@context/vinyl/VinylContext";
 import { checkIsComplete } from "./utils/CheckComplete";
 import type { Vinyl } from "@interfaces/Vinyl";
+import { useUserContext } from "@context/users/UserContext";
+import Loading from "@components/ui/Loading";
+import { DefaultSettings, type VinylSettings } from "@interfaces/UserSettings";
+import { useMemo } from "react";
+
+const tableStyles = {
+  border: 0,
+  '& .row--incomplete': {
+    backgroundColor: 'rgba(211, 47, 47, 0.08)',
+    '&:hover': { backgroundColor: 'rgba(211, 47, 47, 0.12)' },
+  },
+  '& .row--incomplete .MuiDataGrid-cell[data-field="isComplete"]': {
+    color: '#d32f2f',
+    fontWeight: 'bold',
+  }
+};
 
 const VinylsTable = () => {
-  const { vinyls } = useVinylContext();
+  const {isLoading, vinyls } = useVinylContext();
   const navigate = useNavigate();
+  const {isLoading: isSettingsLoading, getCurrentUserSettings, updateCurrentUserSettings} = useUserContext();
+  
+  const initialVisibilityState = useMemo(() => 
+    getCurrentUserSettings()?.vinyls ?? DefaultSettings.vinyls, 
+  [getCurrentUserSettings]);
 
-  const initialVisibilityState = {
-    purchaseNumber: true,
-    artist: true,
-    album: true,
-    owners: true,
-    purchasedBy: false,
-    purchaseDate: false,
-    purchaseLocation: false,
-    price: false,
-    length: false,
-    playCount: false,
-    likedBy: false,
-    notes: false,
-    color: false,
-    doubleLP: false,
-    isComplete: false, 
+  if (isLoading || isSettingsLoading) return <Loading />;
+
+  const processVisibilityChange = (newModel: GridColumnVisibilityModel) => {
+    updateCurrentUserSettings({
+      vinyls: newModel as VinylSettings
+    });
   };
 
   return (
@@ -34,26 +45,12 @@ const VinylsTable = () => {
       onRowClick={(params) => {
         navigate(`/vinyls/${params.id}`);
       }}
+      onColumnVisibilityModelChange={processVisibilityChange}
       autoHeight
-      // Apply the 'row--incomplete' class if the helper returns false
       getRowClassName={(params: GridRowClassNameParams<Vinyl>) => 
         checkIsComplete(params.row) ? '' : 'row--incomplete'
       }
-      sx={{ 
-        border: 0,
-        // Style for the incomplete rows
-        '& .row--incomplete': {
-          backgroundColor: 'rgba(211, 47, 47, 0.08)', // Light red tint
-          '&:hover': {
-            backgroundColor: 'rgba(211, 47, 47, 0.12)',
-          },
-        },
-        // Optional: ensure the Boolean checkbox or text in 'isComplete' is distinct
-        '& .row--incomplete .MuiDataGrid-cell[data-field="isComplete"]': {
-          color: '#d32f2f',
-          fontWeight: 'bold',
-        }
-      }}
+      sx={tableStyles}
       initialState={{
         columns: { columnVisibilityModel: initialVisibilityState },
         sorting: {
