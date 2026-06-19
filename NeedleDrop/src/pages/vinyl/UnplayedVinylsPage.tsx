d84909f4837448e0aaf4a40a271a0eb05413ca0d
Roy Gero <roy.gero.ii@gmail.com>
2026-06-18 22:54:52 -0400

    Adding the header options to the Unplayed Vinyl page.

--- NeedleDrop/src/pages/vinyl/UnplayedVinylsPage.tsx
+++ NeedleDrop/src/pages/vinyl/UnplayedVinylsPage.tsx
@@ -1,9 +1,23 @@
+import ColumnFilterButton from "@components/ui/tables/ColumnFilterButton";
+import ColumnVisibilityButton from "@components/ui/tables/ColumnVisibilityButton";
 import DataTablePage from "@components/ui/DataTablePage";
 import UnplayedVinylsTable from "@components/vinyls/UnplayedVinylsTable";
+import vinylColumns from "@components/vinyls/VinylsTableColumns";
 
 const UnplayedVinylsPage = () => {
   return (
-    <DataTablePage title="Unplayed Vinyls">
+    <DataTablePage
+      title="Vinyls"
+      headerActions={(
+        <>
+          <ColumnVisibilityButton
+            columns={vinylColumns}
+            settingsColumn="vinyls"
+          />
+          <ColumnFilterButton columns={vinylColumns} />
+        </>
+      )}
+    >
       <UnplayedVinylsTable/>
     </DataTablePage>
   )
